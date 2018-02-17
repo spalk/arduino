9ed4449316b4eb5c8473d1731daca7adff1d0e10
demetrius <d.natxa@gmail.com>
2018-02-17 19:21:07 +0300

    tabs interface (graphic only)

--- BME280_to_Nokia5110.ino
+++ BME280_to_Nokia5110.ino
@@ -26,12 +26,17 @@ int lastBacklightButtonState = 0;
 long interval = 5000;
 long previousMillis = 0;
 
+// Lines x-coordinates
+int x_tempr = 18;
+int x_press = 28;
+int x_humid = 38;
+
 void setup() {
   lcd.begin();
   lcd.setContrast(60);
   lcd.clearDisplay();
   Wire.begin();
-  delay(1000);
+  delay(500);
   bme.begin();
   Serial.begin(9600);
 
@@ -63,23 +68,55 @@ void loop(void) {
 
   unsigned long currentMillis = millis();
   if(currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis;   
-  
+    previousMillis = currentMillis;
+    
+    // Frame
+    lcd.drawRect(1, 1, 83, 47, BLACK);
+    
+    // Menu black background
+    lcd.fillRect(2, 2, 82, 13, BLACK);
+
+    // Menu vertical separators
+    lcd.drawLine(18, 2, 18, 13, WHITE);
+    lcd.drawLine(34, 2, 34, 13, WHITE);
+    lcd.drawLine(50, 2, 50, 13, WHITE);
+    lcd.drawLine(66, 2, 66, 13, WHITE);
+
+    // Active white rect
+    lcd.fillRect(2, 2, 17, 13, WHITE);
+
     lcd.setTextSize(1);
-    lcd.setCursor(8,1);
-    lcd.print(bme.readPressure()/133.3);lcd.print("  mmHg");
-    lcd.setCursor(8,17);
-    lcd.print(bme.readTemperature());
-    lcd.setCursor(69,14);
+
+    // Menu Symbols
+    set_text( 6, 5, "#", BLACK);
+    set_text(23, 5, "t", WHITE);
+    set_text(39, 5, "P", WHITE);
+    set_text(55, 5, "H", WHITE);
+    set_text(71, 5, "Y", WHITE);
+
+
+    lcd.setTextColor(BLACK);
+
+    // Temperature line
+    lcd.setCursor(8, x_tempr);
+    lcd.print(bme.readTemperature()); 
+    lcd.setCursor(50, x_tempr);
     lcd.print((char)9);lcd.print("C");
-    lcd.setCursor(8,34);
+
+    // Pressure line
+    lcd.setCursor(8,x_press);
+    lcd.print(bme.readPressure()/133.3);lcd.print(" mmHg");
+
+    // Humidity line
+    lcd.setCursor(8,x_humid);
     lcd.print(bme.readHumidity());
-    set_text(69,34,"%",BLACK);
+    set_text(50,x_humid,"%",BLACK);
+
     lcd.clearDisplay();
   }
 }
 
-void set_text(int x,int y,String text,int color){
+void set_text(int x, int y, String text, int color){
   lcd.setTextColor(color);
   lcd.setCursor(x,y);
   lcd.println(text);
